a5f3259709e2e776f97f2bb436b334b943e80c47
Aneesha <aneeshavarma07@gmail.com>
2025-06-02 10:53:54 +0530

    Added Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'; // or wherever your Tailwind file is
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
